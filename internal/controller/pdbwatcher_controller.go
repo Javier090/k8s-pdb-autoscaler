cfa5a41c37cc4aa66431394e1b8b485e111d1cf6
Javier090 <javiergarc161@gmail.com>
2024-06-04 10:55:43 -0700

    Initial version of controller

--- internal/controller/pdbwatcher_controller.go
+++ internal/controller/pdbwatcher_controller.go
@@ -1,30 +1,19 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package controller
+package controllers
 
 import (
 	"context"
+	"fmt"
 
+	appsv1 "k8s.io/api/apps/v1"
+	policyv1 "k8s.io/api/policy/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	appsv1 "github.com/Javier090/k8s-pdb-autoscaler/api/v1"
+	myappsv1 "github.com/Javier090/k8s-pdb-autoscaler/api/v1"
 )
 
 // PDBWatcherReconciler reconciles a PDBWatcher object
@@ -37,26 +26,52 @@ type PDBWatcherReconciler struct {
 // +kubebuilder:rbac:groups=apps.mydomain.com,resources=pdbwatchers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps.mydomain.com,resources=pdbwatchers/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the PDBWatcher object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.2/pkg/reconcile
 func (r *PDBWatcherReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the PDBWatcher instance
+	pdbWatcher := &myappsv1.PDBWatcher{}
+	err := r.Get(ctx, req.NamespacedName, pdbWatcher)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Fetch the PDB
+	pdb := &policyv1.PodDisruptionBudget{}
+	err = r.Get(ctx, types.NamespacedName{Name: pdbWatcher.Name, Namespace: pdbWatcher.Namespace}, pdb)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Check the DisruptionsAllowed field
+	if pdb.Status.DisruptionsAllowed == 0 {
+		// Fetch the Deployment
+		deployment := &appsv1.Deployment{}
+		err = r.Get(ctx, types.NamespacedName{Name: pdbWatcher.Name, Namespace: pdbWatcher.Namespace}, deployment)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// Scale up the Deployment
+		newReplicas := *deployment.Spec.Replicas + 1
+		deployment.Spec.Replicas = &newReplicas
+		err = r.Update(ctx, deployment)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// Log the scaling action
+		logger.Info(fmt.Sprintf("Scaled up Deployment %s/%s to %d replicas", deployment.Namespace, deployment.Name, newReplicas))
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *PDBWatcherReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appsv1.PDBWatcher{}).
+		For(&myappsv1.PDBWatcher{}).
 		Complete(r)
 }
