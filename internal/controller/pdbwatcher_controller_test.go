5a48846ef259de1b6859c344b9ae5c00a6ea5209
Javier090 <javiergarc161@gmail.com>
2024-06-07 14:06:14 -0700

    Fixed pdb_controllertest.go, issue with test file not compiling due to package not being called correctly

--- internal/controller/pdbwatcher_controller_test.go
+++ internal/controller/pdbwatcher_controller_test.go
@@ -1,74 +1,118 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1" // Import corev1 package
+	policyv1 "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	appsv1 "github.com/Javier090/k8s-pdb-autoscaler/api/v1"
+	v1 "github.com/Javier090/k8s-pdb-autoscaler/api/v1"
 )
 
 var _ = Describe("PDBWatcher Controller", func() {
 	Context("When reconciling a resource", func() {
 		const resourceName = "test-resource"
+		const namespace = "default"
 
 		ctx := context.Background()
-
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO: Modify as needed
-		}
-		pdbwatcher := &appsv1.PDBWatcher{}
+		typeNamespacedName := types.NamespacedName{Name: resourceName, Namespace: namespace}
 
 		BeforeEach(func() {
 			By("creating the custom resource for the Kind PDBWatcher")
+			pdbwatcher := &v1.PDBWatcher{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      resourceName,
+					Namespace: namespace,
+				},
+				Spec: v1.PDBWatcherSpec{
+					// Add relevant spec fields if any
+				},
+			}
 			err := k8sClient.Get(ctx, typeNamespacedName, pdbwatcher)
 			if err != nil && errors.IsNotFound(err) {
-				resource := &appsv1.PDBWatcher{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO: Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+				Expect(k8sClient.Create(ctx, pdbwatcher)).To(Succeed())
 			}
+
+			By("creating a Deployment resource")
+			deployment := &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "example-deployment",
+					Namespace: namespace,
+				},
+				Spec: appsv1.DeploymentSpec{
+					Replicas: int32Ptr(2),
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "example",
+						},
+					},
+					Template: corev1.PodTemplateSpec{ // Use corev1.PodTemplateSpec
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"app": "example",
+							},
+						},
+						Spec: corev1.PodSpec{ // Use corev1.PodSpec
+							Containers: []corev1.Container{ // Use corev1.Container
+								{
+									Name:  "nginx",
+									Image: "nginx:latest",
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, deployment)).To(Succeed())
+
+			By("creating a PDB resource")
+			pdb := &policyv1.PodDisruptionBudget{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "example-pdb",
+					Namespace: namespace,
+				},
+				Spec: policyv1.PodDisruptionBudgetSpec{
+					MinAvailable: &intstr.IntOrString{
+						IntVal: 1,
+					},
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "example",
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, pdb)).To(Succeed())
 		})
 
 		AfterEach(func() {
-			// TODO: Cleanup logic after each test, like removing the resource instance.
-			resource := &appsv1.PDBWatcher{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+			By("cleaning up resources")
+			deleteResource := func(obj client.Object) {
+				Expect(k8sClient.Delete(ctx, obj)).To(Succeed())
+				Eventually(func() bool {
+					err := k8sClient.Get(ctx, client.ObjectKeyFromObject(obj), obj)
+					return errors.IsNotFound(err)
+				}, time.Second*10, time.Millisecond*250).Should(BeTrue())
+			}
 
-			By("Cleanup the specific resource instance PDBWatcher")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+			deleteResource(&v1.PDBWatcher{ObjectMeta: metav1.ObjectMeta{Name: resourceName, Namespace: namespace}})
+			deleteResource(&appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "example-deployment", Namespace: namespace}})
+			deleteResource(&policyv1.PodDisruptionBudget{ObjectMeta: metav1.ObjectMeta{Name: "example-pdb", Namespace: namespace}})
 		})
 
 		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
+			By("reconciling the created resource")
 			controllerReconciler := &PDBWatcherReconciler{
 				Client: k8sClient,
 				Scheme: k8sClient.Scheme(),
@@ -78,6 +122,21 @@ var _ = Describe("PDBWatcher Controller", func() {
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
+
+			// Verify PDBWatcher resource
+			pdbwatcher := &v1.PDBWatcher{}
+			err = k8sClient.Get(ctx, typeNamespacedName, pdbwatcher)
+			Expect(err).NotTo(HaveOccurred())
+
+			// Verify Deployment scaling if necessary
+			deployment := &appsv1.Deployment{}
+			err = k8sClient.Get(ctx, types.NamespacedName{Name: "example-deployment", Namespace: namespace}, deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(*deployment.Spec.Replicas).To(Equal(int32(2))) // Change as needed to verify scaling
 		})
 	})
 })
+
+func int32Ptr(i int32) *int32 {
+	return &i
+}
