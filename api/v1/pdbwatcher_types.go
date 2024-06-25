b665f4a5a4caf3aea8d061bd08dbe6c1dc2bbb2e
Javier090 <javiergarc161@gmail.com>
2024-06-25 15:08:57 -0700

    Modified webhook crd and Struct of PDB controller

--- api/v1/pdbwatcher_types.go
+++ api/v1/pdbwatcher_types.go
@@ -1,41 +1,23 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // PDBWatcherSpec defines the desired state of PDBWatcher
 type PDBWatcherSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of PDBWatcher. Edit pdbwatcher_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	PDBName        string `json:"pdbName"`
+	DeploymentName string `json:"deploymentName"`
+	ScaleFactor    int32  `json:"scaleFactor"`
+	MinReplicas    int32  `json:"minReplicas"`
+	MaxReplicas    int32  `json:"maxReplicas"`
 }
 
 // PDBWatcherStatus defines the observed state of PDBWatcher
 type PDBWatcherStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	CurrentReplicas    int32  `json:"currentReplicas"`
+	DisruptionsAllowed int32  `json:"disruptionsAllowed"`
+	Error              string `json:"error,omitempty"`
 }
 
 // +kubebuilder:object:root=true
